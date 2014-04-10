ae5ccfb90d93a553f238e72e4b296fbbda8b4170
Jakub Malczak <jmalczak@gmail.com>
2014-04-10 23:24:05 +0200

    Added DeezerRec draft

---
+++ DeezerRec/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using Nancy.Hosting.Self;
+using NAudio.Wave;
+
+namespace DeezerRec
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public NancyHost NancyHost { get; set; }
+        public static WasapiLoopbackCapture WaveIn { get; set; }
+        public static WaveFileWriter WaveFileWritter { get; set; }
+        public static string FileName { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            NancyHost = new NancyHost(new Uri("http://localhost:8080/"));
+            NancyHost.Start();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            NancyHost.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
---
+++ DeezerRec/DeezerLocalModule.cs
@@ -0,0 +1,42 @@
+﻿using Nancy;
+using NAudio.Wave;
+
+namespace DeezerRec
+{
+    public class DeezerLocalModule : NancyModule
+    {
+        public DeezerLocalModule()
+        {
+            Get["/"] = param => View["Player.html"];
+            Get["/Include"] = param => View["Include.html"];
+            Get["/Start/{band}/{title}"] = _ =>
+            {
+                bool firstRun = App.WaveFileWritter == null;
+
+                if (!firstRun)
+                {
+                    App.WaveFileWritter.Dispose();
+                }
+                
+                App.FileName = string.Format(@"C:\{0} - {1}.wav", _.band, _.title);
+                App.WaveFileWritter = new WaveFileWriter(App.FileName, App.WaveIn.WaveFormat);
+
+                App.WaveIn.StartRecording();
+
+                return string.Empty;
+            };
+            Get["/End"] = _ =>
+            {
+                App.WaveIn.StopRecording();
+                App.WaveIn.RecordingStopped += WaveIn_RecordingStopped;
+
+                return string.Empty;
+            };
+        }
+        
+        void WaveIn_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            App.WaveFileWritter.Dispose();
+        }
+    }
+}
